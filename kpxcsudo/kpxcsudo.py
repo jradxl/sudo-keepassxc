a253217dd3c58fa348a7656d48d094ada7c2d7d3
John Radley <jradxl@gmail.com>
2026-07-13 21:23:12 +0200

    Updates

--- kpxcsudo/kpxcsudo.py
+++ kpxcsudo/kpxcsudo.py
@@ -1,15 +1,28 @@
-import pykeepass, getpass, platform
+import sys, pykeepass, getpass, platform
 
 def main():
     password = getpass.getpass("Your KeepassXC Password: ")
-    kp = pykeepass.PyKeePass('/home/jradley/.keepassxc/db.kdbx', password=password)
+    try:
+        kp = pykeepass.PyKeePass('/home/jradley/.keepassxc/db.kdbx', password=password)
+    except Exception as err:
+        print("ERROR:", err)
+        sys.exit(1)
 
     hostname = platform.node()
     username = getpass.getuser()
 
     #Find an entry by its title, where that is the hostname
-    entry = kp.find_entries(title=hostname, username=username, first=True)
-    print(entry.password)
+    try:
+        entry = kp.find_entries(title=hostname, username=username, first=True)
+    except Exception as err:
+        print("ERROR: ", err)
+        sys.exit(1)
+
+    if entry is None:
+        print("ERROR: No entry found in database.")
+        sys.exit(1)
+    else:
+        print(entry.password)
 
 if __name__ == '__main__':
     main()
